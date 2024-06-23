524920cb616abcc3146df99ff3da6de5c86f26ef
sourcedoradus <valentine.medvedev@gmail.com>
2024-06-23 20:25:09 -0300

    Added counter's button, label and logic

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func plusOneAction(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "Значение счётчика: \(counter)"
+    }
 }
+    
 
