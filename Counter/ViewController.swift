f3eb38fccfdd164d5464bdccb8c042b5bc6b56ec
sourcedoradus <valentine.medvedev@gmail.com>
2024-06-24 14:04:29 -0300

    added history log

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var counterMinusButton: UIButton!
     @IBOutlet weak var counterLabel: UILabel!
     @IBOutlet weak var cleanButton: UIButton!
+    @IBOutlet weak var counterHistory: UITextView!
+    
     var counter = 0
     
     override func viewDidLoad() {
@@ -24,18 +26,35 @@ class ViewController: UIViewController {
         if counter != 0 {
             counter = 0 }
         counterLabel.text = "Значение счётчика: \(counter)"
+        counterHistoryLog(message: "значение сброшено")
     }
     
     @IBAction func minusOneAction(_ sender: Any) {
         if counter > 0 {
-            counter -= 1 }
+            counter -= 1
+            counterHistoryLog(message: "значение изменено на -1")
+        } else {
+            counterHistoryLog(message: "попытка уменьшить значение счётчика ниже 0")
+        }
         counterLabel.text = "Значение счётчика: \(counter)"
     }
     
     @IBAction func plusOneAction(_ sender: Any) {
         counter += 1
         counterLabel.text = "Значение счётчика: \(counter)"
+        counterHistoryLog(message: "значение изменено на +1")
+    }
+    
+    func counterHistoryLog(message: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let timestamp = dateFormatter.string(from: Date())
+        
+        let logEntry = "[\(timestamp)]: \(message)\n"
+        counterHistory.text.append(logEntry)
+        
+        let range = NSRange(location: counterHistory.text.count - 1, length: 0)
+        counterHistory.scrollRangeToVisible(range)
     }
 }
-    
 
