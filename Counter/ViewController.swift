c99d91093c5f116a88f5e5600831e15e8f83a039
sourcedoradus <valentine.medvedev@gmail.com>
2024-06-23 21:07:12 -0300

    added clear and minus buttons

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,14 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterPlusButton: UIButton!
+    @IBOutlet weak var counterMinusButton: UIButton!
     @IBOutlet weak var counterLabel: UILabel!
-    
+    @IBOutlet weak var cleanButton: UIButton!
     var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func clearAction(_ sender: Any) {
+        if counter != 0 {
+            counter = 0 }
+        counterLabel.text = "Значение счётчика: \(counter)"
+    }
+    
+    @IBAction func minusOneAction(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1 }
+        counterLabel.text = "Значение счётчика: \(counter)"
     }
     
     @IBAction func plusOneAction(_ sender: Any) {
