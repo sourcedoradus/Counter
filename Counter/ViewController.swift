a5bb939d49a4d96aa61e7380d17818bd4bcd37c7
sourcedoradus <valentine.medvedev@gmail.com>
2024-06-29 12:34:43 -0300

    added modifiers for optimisation and security

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,27 +1,27 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
-    @IBOutlet weak var counterPlusButton: UIButton!
-    @IBOutlet weak var counterMinusButton: UIButton!
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var cleanButton: UIButton!
-    @IBOutlet weak var counterHistory: UITextView!
+    @IBOutlet private weak var counterPlusButton: UIButton!
+    @IBOutlet private weak var counterMinusButton: UIButton!
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var cleanButton: UIButton!
+    @IBOutlet private weak var counterHistory: UITextView!
     
-    var counter = 0
+    private var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func cleanAction(_ sender: Any) {
+    @IBAction private func cleanAction(_ sender: Any) {
         if counter != 0 {
             counter = 0 }
         counterLabel.text = "Значение счётчика: \(counter)"
         counterHistoryLog(message: "значение сброшено")
     }
     
-    @IBAction func minusOneAction(_ sender: Any) {
+    @IBAction private func minusOneAction(_ sender: Any) {
         if counter > 0 {
             counter -= 1
             counterHistoryLog(message: "значение изменено на -1")
@@ -31,13 +31,13 @@ class ViewController: UIViewController {
         counterLabel.text = "Значение счётчика: \(counter)"
     }
     
-    @IBAction func plusOneAction(_ sender: Any) {
+    @IBAction private func plusOneAction(_ sender: Any) {
         counter += 1
         counterLabel.text = "Значение счётчика: \(counter)"
         counterHistoryLog(message: "значение изменено на +1")
     }
     
-    func counterHistoryLog(message: String) {
+    private func counterHistoryLog(message: String) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         let timestamp = dateFormatter.string(from: Date())
